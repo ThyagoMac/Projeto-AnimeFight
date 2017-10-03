319a64b0f71db8db355e56398cb2eaaa5755f697
Thyago Mac Cormick <32471417+ThyagoMac@users.noreply.github.com>
2017-10-03 15:32:42 -0300

    Update scriptall.js

--- js/scriptall.js
+++ js/scriptall.js
@@ -2,6 +2,138 @@ $(".corpo").submit(function (event) {
   event.preventDefault(); //impede que a pag recarregue qnd clica no submit
 
 });
+$(document).ready(function () {
+
+  var qualidades = $('.perks').clone();
+  $(qualidades).insertAfter('.perks');
+  $(qualidades).insertAfter('.perks');
+  $(qualidades).insertAfter('.perks');
+
+  var segundaFicha = $('.ficha').clone();
+  $(segundaFicha).insertAfter('.ficha');
+
+  $('input[type="range"]').change(function () {
+    var box = $(this).parents('tr').find('input[type="text"]');
+    $(box).val($(this).val());
+    $(box).trigger('change');
+    ////Catar as variaveis e joga no box de total
+    var str = $(this).parents('section').find('.box-forca').val();
+    var con = $(this).parents('section').find('.box-con').val();
+    var dex = $(this).parents('section').find('.box-dex').val();
+    var agi = $(this).parents('section').find('.box-agi').val();
+    var int = $(this).parents('section').find('.box-mag').val();
+    var total = parseInt(str) + parseInt(con) + parseInt(dex) + parseInt(agi) + parseInt(int);
+    var box = $(this).parents('section').find('.box-total');
+    $(box).val(total);
+    ////Atribui o dano = (str*2)+int
+    var box = $(this).parents('section').find('.box-dano');
+    var str = $(this).parents('section').find('.box-forca').val();
+    var int = $(this).parents('section').find('.box-mag').val();
+    var bonus = (parseInt(str) * 2) + parseInt(int);
+    $(box).val(bonus);
+  });
+
+  $(".box-con").change(function () {
+    var box = $(this).parents('section').find('.box-hp');
+    var bonus = $(this).val() * 10 + 10;
+    $(box).val(bonus);
+  });
+
+  $(".box-dex").change(function () {
+    var box = $(this).parents('section').find('.box-acerto');
+    var bonus = $(this).val() * 5;
+    $(box).val(bonus);
+  });
+
+  $(".box-agi").change(function () {
+    var box = $(this).parents('section').find('.box-def');
+    var bonus = $(this).val() * 5;
+    $(box).val(bonus);
+  });
+
+  $('select[class="perks"]').change(function () {
+    var perk = $(this).val();
+    switch (perk) {
+      case "voar":
+        var box = $(this).parents('section').find('.box-def');
+        var boxval = $(this).parents('section').find('.box-def').val() * 1 + 8;
+        $(box).val(boxval);
+        var box = $(this).parents('section').find('.box-acerto');
+        var boxval = $(this).parents('section').find('.box-acerto').val() * 1 + 8;
+        $(box).val(boxval);
+        break;
+
+      case "danoMagic":
+        var danoMagicoTotal = 4;
+        break;
+
+      case "controle":
+        var box = $(this).parents('section').find('.box-def');
+        var boxval = $(this).parents('section').find('.box-def').val() * 1 + 5;
+        $(box).val(boxval);
+        var box = $(this).parents('section').find('.box-acerto');
+        var boxval = $(this).parents('section').find('.box-acerto').val() * 1 + 10;
+        $(box).val(boxval);
+        break;
+
+      case "teleport":
+        var box = $(this).parents('section').find('.box-def');
+        var boxval = $(this).parents('section').find('.box-def').val() * 1 + 15;
+        $(box).val(boxval);
+        $(box).trigger('change');
+        break;
+
+      case "armor":
+        console.log("test5");
+        break;
+
+      case "arma":
+        console.log("test6");
+        break;
+    }
+  });
+
+  ///Cod Hover!!!
+  $(".forca")
+    .mouseenter(function () {
+      $("textarea[name='combatText']").val("FORÇA: Determina o dano base que seu personagem consegue causar em combate corpo a corpo. (Aumenta o dano base em 2 ptos)");
+    })
+    .mouseleave(function () {
+      $("textarea[name='combatText']").val('');
+    });
+  $(".const")
+    .mouseenter(function () {
+      $("textarea[name='combatText']").val("CONSTITUIÇÃO: Determina o quanto seu personagem consegue resistir antes de cair. (Aumenta o HP em 5 ptos)");
+    })
+    .mouseleave(function () {
+      $("textarea[name='combatText']").val('');
+    });
+  $(".destreza")
+    .mouseenter(function () {
+      $("textarea[name='combatText']").val("DESTREZA: Determina a precição do seu personagem. (Aumenta a chance de acertar em 5%)");
+    })
+    .mouseleave(function () {
+      $("textarea[name='combatText']").val('');
+    });
+  $(".agilit")
+    .mouseenter(function () {
+      $("textarea[name='combatText']").val("AGLIDADE: Determina a velocidade do seu personagem. (Aumenta a chance de esquiva em 5%)");
+    })
+    .mouseleave(function () {
+      $("textarea[name='combatText']").val('');
+    });
+  $(".magia")
+    .mouseenter(function () {
+      $("textarea[name='combatText']").val("MAGIA: Determina o dano a distância que seu personagem pode causar. (Aumenta o dano do combate a distancia em 2 ptos)");
+    })
+    .mouseleave(function () {
+      $("textarea[name='combatText']").val('');
+    });
+  /////
+
+});
+
+
 
 // $(document).ready(function () { //starta quando a pag carrega
 //   $("#priFor").change(function () { //trigga quando muda o input range de ID aproFor
@@ -94,94 +226,3 @@ $(".corpo").submit(function (event) {
 //     $("#boxMag2").val() * 1;
 //   $("#secTotal").val(total2);
 // });
-$(document).ready(function () {
-
-  var qualidades = $('.perks').clone();
-  $(qualidades).insertAfter('.perks');
-  $(qualidades).insertAfter('.perks');
-  $(qualidades).insertAfter('.perks');
-
-  var segundaFicha = $('.ficha').clone();
-  $(segundaFicha).insertAfter('.ficha');
-
-  $('input[type="range"]').change(function () {
-    var box = $(this).parents('tr').find('input[type="text"]');
-    $(box).val($(this).val());
-    $(box).trigger('change');
-
-    var total1 = $(".box-forca").val() * 1 +
-      $(".box-con").val() * 1 +
-      $(".box-dex").val() * 1 +
-      $(".box-agi").val() * 1 +
-      $(".box-mag").val() * 1;
-    $(".box-total").val(total1);
-  });
-
-  $(".box-forca").change(function () {
-    var box = $(this).parents('section').find('.box-dano');
-    var bonus = $(this).val() * 2;
-    $(box).val(bonus);
-  });
-
-  $(".box-con").change(function () {
-    var box = $(this).parents('section').find('.box-hp');
-    var bonus = $(this).val() * 5 + 10;
-    $(box).val(bonus);
-  });
-
-  $(".box-dex").change(function () {
-    var box = $(this).parents('section').find('.box-acerto');
-    var bonus = $(this).val() * 5;
-    $(box).val(bonus);
-  });
-
-  $(".box-agi").change(function () {
-    var box = $(this).parents('section').find('.box-def');
-    var bonus = $(this).val() * 5;
-    $(box).val(bonus);
-  });
-
-  //$(".box-mag").change(function(){
-  //  var bonus = $(this).val();
-  //});
-
-  ///Cod Hover!!!
-  $(".forca")
-    .mouseenter(function () {
-      $("textarea[name='combatText']").val("FORÇA: Determina o dano base que seu personagem consegue causar em combate corpo a corpo. (Aumenta o dano base em 2 ptos)");
-    })
-    .mouseleave(function () {
-      $("textarea[name='combatText']").val('');
-    });
-  $(".const")
-    .mouseenter(function () {
-      $("textarea[name='combatText']").val("CONSTITUIÇÃO: Determina o quanto seu personagem consegue resistir antes de cair. (Aumenta o HP em 5 ptos)");
-    })
-    .mouseleave(function () {
-      $("textarea[name='combatText']").val('');
-    });
-  $(".destreza")
-    .mouseenter(function () {
-      $("textarea[name='combatText']").val("DESTREZA: Determina a precição do seu personagem. (Aumenta a chance de acertar em 5%)");
-    })
-    .mouseleave(function () {
-      $("textarea[name='combatText']").val('');
-    });
-  $(".agilit")
-    .mouseenter(function () {
-      $("textarea[name='combatText']").val("AGLIDADE: Determina a velocidade do seu personagem. (Aumenta a chance de esquiva em 5%)");
-    })
-    .mouseleave(function () {
-      $("textarea[name='combatText']").val('');
-    });
-  $(".magia")
-    .mouseenter(function () {
-      $("textarea[name='combatText']").val("MAGIA: Determina o dano a distância que seu personagem pode causar. (Aumenta o dano do combate a distancia em 2 ptos)");
-    })
-    .mouseleave(function () {
-      $("textarea[name='combatText']").val('');
-    });
-  /////
-
-
-});
